58b8d66ceeb062950123af0550688e6952691df5
vdonoladev <victordonolaf@gmail.com>
2020-10-19 08:28:37 -0300

    Updates

--- content.js
+++ content.js
@@ -1,25 +1,22 @@
 const interval = setInterval(() => {
-	const header = document.querySelector("._1QUKR");
-	if(header){
-		console.log(header)
-		clearInterval(interval)
+  const header = document.querySelector('._1QUKR');
+  if (header) {
+    console.log(header);
+    clearInterval(interval);
 
+    const button = document.createElement('button');
+    button.innerHTML = '2x';
+    button.classList.add('twoTimesButton');
 
-		const button = document.createElement("button")
-		button.innerHTML = "2x"
-		button.classList.add("twoTimesButton")
+    button.addEventListener('click', () => {
+      const audios = document.querySelectorAll('audio');
+      console.log(audios);
+      audios.forEach((audio) => {
+        console.log(audio);
+        audios.playbackRate = 2;
+      });
+    });
 
-		button.addEventListener("click", () => {
-			const audios = document.querySelectorAll("audio");
-			console.log(audios)
-			audios.forEach((audio) => {
-				console.log(audio)
-				audios.playbackRate = 2;
-			})
-		})
-
-
-		header.appendChild(button)
-
-	}
-}, 1000)
+    header.appendChild(button);
+  }
+}, 1000);
